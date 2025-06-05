fd71f0ac6f9e549e68d74e3c879241d5389c04c0
marselmuleri <165370205+marselmuleri@users.noreply.github.com>
2025-06-05 15:52:04 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,13 @@
 <?php
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\IstilahController;
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Auth\ForgotPasswordController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('Home');
-});
-
-Route::get('/search', function (Request $request) {
-    $query = $request->input('query');
-    return "You searched for: " . $query;
-})->name('search');
-
-Route::get('/AboutUs', function () {
-    return view('AboutUs');
-});
-
-Route::get('/Glosarium', function () {
-    return view('Glosarium');
-});
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/register',function () {
-    return view('register');
-});
-
-Route::get('/Istilah',function () {
-    return view('Istilah');
-});
+Route::get('/', [IstilahController::class, 'index']);
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/signup', [AuthController::class, 'showSignUp']);
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/istilah/{nama_istilah}', [IstilahController::class, 'showIstilah']);
+Route::post('/search', [IstilahController::class, 'search']);
+Route::get('/glossary', [IstilahController::class, 'glossary']);
